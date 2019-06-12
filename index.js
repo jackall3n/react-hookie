f91eedee8d9550ba3f5e6e2d5e8d06e9f1b1a4e8
Jack Allen <jackallensemail+git@gmail.com>
2019-06-12 11:16:12 +0100

    build

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var useHookie = function (configuration, reactUseState) {
     return Object.keys(configuration).reduce(function (config, key) {
+        var _a;
         var initial_value = configuration[key];
-        var _a = reactUseState(initial_value), value = _a[0], set = _a[1];
-        config[key] = {
+        var _b = reactUseState(initial_value), value = _b[0], set = _b[1];
+        return __assign({}, config, (_a = {}, _a[key] = {
             _: value,
             set: set
-        };
+        }, _a));
     }, {});
 };
 exports.default = useHookie;
